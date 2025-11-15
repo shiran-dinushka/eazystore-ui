9781dd8628ca47c17232217946e610531673efcf
shiran-dinushka <shirandinushka9@gmail.com>
2025-11-15 17:27:19 +0530

    add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { Footer } from "./components/footer/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import "./Footer.css";
+
+export function Footer() {
+  return (
+    <footer className="footer">
+      Built with 
+      <FontAwesomeIcon 
+      icon={faHeart} 
+      className="footer-icon" 
+      aria-hidden="true"/> 
+      by Eazy Stickers
+    </footer>
+  );
+}
