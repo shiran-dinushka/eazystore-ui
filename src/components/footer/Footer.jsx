ae058977c6451eb1385fcde8f17300bef5c24fb2
shiran-dinushka <shirandinushka9@gmail.com>
2025-11-20 12:59:41 +0530

    Remove css styling and configure tailwindcss to be implement

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,15 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import "./Footer.css";
 
 export function Footer() {
   return (
     <footer className="footer">
-      Built with 
-      <FontAwesomeIcon 
-      icon={faHeart} 
-      className="footer-icon" 
-      aria-hidden="true"/> 
+      Built with
+      <FontAwesomeIcon
+        icon={faHeart}
+        className="footer-icon"
+        aria-hidden="true"
+      />
       by Eazy Stickers
     </footer>
   );
