e868bf0b81a3c342d76c92495fd3e287f09f5f99
shiran-dinushka <shirandinushka9@gmail.com>
2025-11-20 14:27:54 +0530

    Design product and cards with tailwindcss

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import products from "../data/product";
 
 export default function Home() {
   return (
-    <div className="home-container">
+    <div className="max-w-6xl mx-auto px-6 py-8">
       <PageHeading title="Explore Eazy Stickers">
         Add a touch of creativity to your space with outr wide range of fun and
         unique stickers. Perfect for any occasion!
