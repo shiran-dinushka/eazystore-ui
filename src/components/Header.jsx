a13d773e250f748dfaa2fed9e5721eeb482184b5
shiran-dinushka <shirandinushka9@gmail.com>
2025-12-15 18:31:06 +0530

    add theme to local storage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,37 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingBasket, faTags, faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import {
+  faShoppingBasket,
+  faTags,
+  faMoon,
+  faSun,
+} from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
 const Header = () => {
-  const [theme, setTheme]= useState("light")
+  const [theme, setTheme] = useState(() => {
+    return localStorage.getItem("theme") === "dark" ? "dark" : "light";
+  });
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
 
   const toggleTheme = () => {
     setTheme((prevTheme) => {
-      const newThem = prevTheme === "light" ? "dark" : "light"
-
-      if (newThem === "dark") {
-        document.documentElement.classList.add("dark");
-      } else {
-        document.documentElement.classList.remove("dark");
-      }
+      const newThem = prevTheme === "light" ? "dark" : "light";
+      localStorage.setItem("theme", newThem);
       return newThem;
-  });
-  }
-  const navLinkClass = "text-center text-lg font-primary font-semibold text-primary py-2 dark:text-light hover:text-dark dark:hover:text-lighter";
+    });
+  };
+  const navLinkClass =
+    "text-center text-lg font-primary font-semibold text-primary py-2 dark:text-light hover:text-dark dark:hover:text-lighter";
   return (
     <header className="border-b border-gray-300 dark:border-gray-600 sticky top-0 z-20 bg-normalbg dark:bg-darkbg">
       <div className="flex items-center justify-between mx-auto max-w-6xl px-6 py-4">
-        <a
-          href="/"
-          className={navLinkClass}
-        >
+        <a href="/" className={navLinkClass}>
           <FontAwesomeIcon icon={faTags} className="h-8 w-8" />
           <span className="font-bold"> Eazy Stickers</span>
         </a>
@@ -40,40 +48,31 @@ const Header = () => {
           </button>
           <ul className="flex space-x-6">
             <li>
-              <a
-                href="/"
-                className={navLinkClass}
-              >
+              <a href="/" className={navLinkClass}>
                 Home
               </a>
             </li>
             <li>
-              <a
-                href="/about"
-                className={navLinkClass}
-              >
+              <a href="/about" className={navLinkClass}>
                 About
               </a>
             </li>
             <li>
-              <a
-                href="/conatct"
-                className={navLinkClass}
-              >
+              <a href="/conatct" className={navLinkClass}>
                 Contact
               </a>
             </li>
             <li>
-              <a
-                href="/login"
-                className={navLinkClass}
-              >
+              <a href="/login" className={navLinkClass}>
                 Login
               </a>
             </li>
             <li>
               <a href="/cart" className="text-primary py-2">
-                <FontAwesomeIcon icon={faShoppingBasket}  className="dark:text-light"/>
+                <FontAwesomeIcon
+                  icon={faShoppingBasket}
+                  className="dark:text-light"
+                />
               </a>
             </li>
           </ul>
