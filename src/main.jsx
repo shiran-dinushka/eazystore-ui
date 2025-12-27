5c6c2d4ba577de37f9439e54937691ad34aa0905
Shiran Dinushka <shirandinushka9@gmail.com>
2025-12-27 16:30:57 +0530

    Change Create Routing method

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, createRoutesFromElements,Route } from "react-router-dom";
 import About from "./components/About.jsx";
 import Contact from "./components/Contact.jsx";
 import Cart from "./components/Cart.jsx";
@@ -10,7 +10,7 @@ import Login from "./components/Login.jsx";
 import Home from "./components/Home.jsx";
 import ErrorPage from "./components/ErrorPage.jsx";
 
-const appRoute = createBrowserRouter([
+/*const appRoute = createBrowserRouter([
   { path: "/", 
     element: <App />, 
     errorElement: <ErrorPage />,
@@ -24,8 +24,19 @@ const appRoute = createBrowserRouter([
   ]},
   
   // Define other routes here as needed
-]);
+]);*/
 
+const routeDefinitions = createRoutesFromElements(
+  <Route path="/" element={<App />} errorElement={<ErrorPage />}>
+    <Route index element={<Home />} />
+    <Route path="/home" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/cart" element={<Cart />} />
+  </Route>
+);
+const appRoute = createBrowserRouter(routeDefinitions);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={appRoute} />
