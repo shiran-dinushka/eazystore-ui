cfb24d963c80e0b0e59fc8be40e2ce6b705d594b
Shiran Dinushka <shirandinushka9@gmail.com>
2025-12-27 16:11:36 +0530

    Fix the error in tiitle

--- src/components/PageHeading.jsx
+++ src/components/PageHeading.jsx
@@ -1,10 +1,10 @@
 import React, { Children } from "react";
-import PageTile from "./PageTile";
+import PageTitle from "./PageTitle";
 
 export default function PageHeading({ title, children }) {
   return (
     <div className="text-center max-w-xl my-6 px-4 mx-auto">
-      <PageTile title={title} />
+      <PageTitle title={title} />
       <p className="font-primary leading-6 text-gray-600 dark:text-lighter">{children}</p>
     </div>
   );
--- src/components/PageTile.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function PageTile({title}) {
-  return (
-    <h1 className="text-3xl font-primary font-extrabold text-center text-primary dark:text-light mt-4 py-2">{title}</h1>
-  );
-}
